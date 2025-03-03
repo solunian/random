5444581de6750ee17df248675b43c44d3658a778
Daniel Cui <solunian@gmail.com>
2025-03-02 18:16:52 -0800

    random stuffs

---
+++ comb.py
@@ -0,0 +1,33 @@
+# lmao cmptgcs2 hw5. just wanted to write some python code. its been too long my bae.
+
+
+def f(n: int) -> int:
+    sum = 1
+    for i in range(1, n + 1):
+        sum *= i
+    return sum
+
+
+def C(n: int, r: int) -> int:
+    comb = f(n) / (f(r) * f(n - r))
+    if comb == int(comb):
+        return int(comb)
+    else:
+        raise Exception("combination gave a non-integer value")
+
+
+print(f"just flavors: {C(28, 3)}")
+
+c_sauces = 0
+for m in range(0, 8 + 1):
+    c_sauces += C(8, m)
+
+c_toppings = 0
+for n in range(0, 12 + 1):
+    c_toppings += C(12, n)
+
+
+print(f"all: {C(28, 3) * c_sauces * c_toppings}")
+
+print(f"just one of everything: {C(28, 1) * C(8, 1) * C(12, 1)}")
+print(f"28 * 8 * 12: {28 * 8 * 12}")
